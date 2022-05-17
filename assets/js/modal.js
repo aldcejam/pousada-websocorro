79887be7522e8790a2df4845229fa6fc0ace5be1
Aldcejam <aldcejamj923@gmail.com>
2022-05-17 05:49:18 -0300

    finaled

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -5,6 +5,7 @@ const buttonCloseModal = document.querySelector('.modal--inter-button-close')
 buttonCloseModal.addEventListener('click', ()=>{
     modal.classList.add('close-modal')
     modal.classList.remove('open-modal')
+    document.querySelector(".body").style.overflowY = "scroll"
 })
 
 
@@ -12,4 +13,5 @@ const buttonOpenModal = document.querySelector('.illustration--mobile-active-3')
 buttonOpenModal.addEventListener('click', ()=>{
     modal.classList.remove('close-modal')
     modal.classList.add('open-modal')
+    document.querySelector(".body").style.overflowY = "hidden"
 })
